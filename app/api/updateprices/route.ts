a9c9a115aef398893a43b4e342d290166de38a60
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2025-04-25 14:03:14 +0300

    added cron job

--- app/api/updateprices/route.ts
+++ app/api/updateprices/route.ts
@@ -1,3 +1,12 @@
 export function GET(request: Request) {
-  return new Response('Price data updated');
+  const authHeader = request.headers.get('authorization');
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response('Unauthorized', {
+      status: 401,
+    });
+  }
+
+  const currentDate = new Date().toLocaleString();
+  console.log(currentDate, "- Vercel cron job running");
+  return new Response("Price data updated");
 }
