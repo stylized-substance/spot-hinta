10bdcf7897bfdb275cc6b176d60fb79369395145
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2025-05-02 13:57:01 +0300

    built date strings for fetch

--- app/api/updateprices/route.ts
+++ app/api/updateprices/route.ts
@@ -1,50 +1,67 @@
-import { XMLParser } from 'fast-xml-parser'
+import { XMLParser } from "fast-xml-parser";
 
 // Fetch price data from ENTSO-E
 export async function GET(request: Request) {
   // Check for existence of Vercel cron secret in authorization header
-  const authHeader = request.headers.get('authorization');
-  if (authHeader !== `Bearer ${process.env.CRON_SECRET}` && process.env.NODE_ENV !== 'development') {
-      return new Response('Unauthorized', {
-          status: 401,
-        });
-      }
+  const authHeader = request.headers.get("authorization");
+  if (
+    authHeader !== `Bearer ${process.env.CRON_SECRET}` &&
+    process.env.NODE_ENV !== "development"
+  ) {
+    return new Response("Unauthorized", {
+      status: 401,
+    });
+  }
 
   if (!process.env.ENTSO_E_APIKEY) {
-    throw new Error('ENTSO-E API key missing')
+    throw new Error("ENTSO-E API key missing");
   }
-  
-  const currentDate = new Date().toLocaleString();
-  console.log(currentDate, "- Fetching price data from ENTSO-E");
 
-  const xmlParser = new XMLParser
+  const xmlParser = new XMLParser();
+
+  // Build date strings that comply with API
+  const today = new Date();
+  const yesterday = new Date();
+  yesterday.setDate(today.getDate() - 1);
+
+  const todayYear = today.getFullYear().toString();
+  const todayMonth = (today.getMonth() + 1).toString().padStart(2, "0");
+  const todayDate = today.getDate().toString().padStart(2, "0");
+  const yesterdayYear = yesterday.getFullYear().toString();
+  const yesterdayMonth = (yesterday.getMonth() + 1).toString().padStart(2, "0");
+  const yesterdayDate = yesterday.getDate().toString().padStart(2, "0");
+
+  const startTime = "2200";
+  const endTime = "2200";
 
   // Set beginning and end time to fetch
-  const periodStart = '202504300000'
-  const periodEnd = '202504302300'
+  const periodStart = `${yesterdayYear}${yesterdayMonth}${yesterdayDate}${startTime}`;
+  const periodEnd = `${todayYear}${todayMonth}${todayDate}${endTime}`;
 
-  const entsoUrl = `https://web-api.tp.entsoe.eu/api?documentType=A44&periodStart=${periodStart}&periodEnd=${periodEnd}&out_Domain=10YFI-1--------U&in_Domain=10YFI-1--------U&securityToken=${process.env.ENTSO_E_APIKEY}`
-  
-  const response = await fetch(
-    entsoUrl,
-    {
-      method: 'GET',
-    }
+  console.log(today.toLocaleString(), "- Fetching price data from ENTSO-E");
+
+  const entsoUrl = `https://web-api.tp.entsoe.eu/api?documentType=A44&periodStart=${periodStart}&periodEnd=${periodEnd}&out_Domain=10YFI-1--------U&in_Domain=10YFI-1--------U&securityToken=${process.env.ENTSO_E_APIKEY}`;
+
+  const response = await fetch(entsoUrl, {
+    method: "GET",
+  });
+
+  const xmlResponse = await response.text();
+  const parsedXml = xmlParser.parse(xmlResponse);
+  const timeSeriesData = Array.isArray(
+    parsedXml.Publication_MarketDocument.TimeSeries,
   )
-
-  const xmlResponse = await response.text()
-  
-  const parsedXml = xmlParser.parse(xmlResponse)
-
-  const { timeInterval, resolution, Point } = parsedXml.Publication_MarketDocument.TimeSeries[0].Period
+    ? parsedXml.Publication_MarketDocument.TimeSeries[0].Period
+    : parsedXml.Publication_MarketDocument.TimeSeries.Period;
+  const { timeInterval, resolution, Point } = timeSeriesData;
 
   const data = {
     timeInterval,
     resolution,
-    Point
-  }
+    Point,
+  };
 
-  console.log(data)
+  console.log('data', data);
 
   return new Response("Price data updated");
 }
