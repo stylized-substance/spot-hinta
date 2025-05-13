957fa561bb25f3d894238442bf41ec600536f065
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2025-05-13 12:14:00 +0300

    moved updatePrices to separate file

---
+++ app/utils/updatePrices.ts
@@ -0,0 +1,101 @@
+import { XMLParser } from "fast-xml-parser";
+import { z } from "zod";
+import sql from "@/app/db/db";
+import { fetchWithRetry } from "@/app/utils/fetchWithRetry";
+
+// Fetch price data from ENTSO-E
+export async function updatePrices() {
+  if (!process.env.ENTSO_E_APIKEY) {
+    throw new Error("ENTSO-E API key missing");
+  }
+
+  const xmlParser = new XMLParser();
+
+  const ApiPriceData = z.object({
+    timeInterval: z.object({
+      start: z.string(),
+      end: z.string(),
+    }),
+    resolution: z.string(),
+    Point: z.array(
+      z.object({
+        position: z.number(),
+        "price.amount": z.number(),
+      }),
+    ),
+  });
+
+  // Build date strings that comply with API
+  const today = new Date();
+  const yesterday = new Date();
+  yesterday.setDate(today.getDate() - 1);
+
+  const todayYear = today.getFullYear().toString();
+  const todayMonth = (today.getMonth() + 1).toString().padStart(2, "0");
+  const todayDate = today.getDate().toString().padStart(2, "0");
+  const yesterdayYear = yesterday.getFullYear().toString();
+  const yesterdayMonth = (yesterday.getMonth() + 1).toString().padStart(2, "0");
+  const yesterdayDate = yesterday.getDate().toString().padStart(2, "0");
+
+  // Set beginning and end time to fetch
+  const periodStart = `${yesterdayYear}${yesterdayMonth}${yesterdayDate}2200`;
+  const periodEnd = `${todayYear}${todayMonth}${todayDate}2200`;
+
+  console.log(today.toLocaleString(), "- Fetching price data from ENTSO-E");
+
+  const entsoUrl = `https://web-api.tp.entsoe.eu/api?documentType=A44&periodStart=${periodStart}&periodEnd=${periodEnd}&out_Domain=10YFI-1--------U&in_Domain=10YFI-1--------U&contract_MarketAgreement.type=A01&securityToken=${process.env.ENTSO_E_APIKEY}`;
+
+  const response = await fetchWithRetry(entsoUrl, 3, {
+    method: "GET",
+  });
+
+  const xmlResponse = await response.text();
+  const parsedXml = xmlParser.parse(xmlResponse);
+
+  const timeSeriesData = Array.isArray(
+    parsedXml.Publication_MarketDocument.TimeSeries,
+  )
+    ? parsedXml.Publication_MarketDocument.TimeSeries[0].Period
+    : parsedXml.Publication_MarketDocument.TimeSeries.Period;
+  const { timeInterval, resolution, Point } = timeSeriesData;
+
+  const priceData = {
+    timeInterval,
+    resolution,
+    Point,
+  };
+
+  console.log("Price data from API:\n", priceData);
+
+  ApiPriceData.parse(priceData);
+
+  // Build price data rows and save to database
+  const hoursArray = [];
+  const firstHour = new Date(priceData.timeInterval.start);
+  firstHour.setHours(firstHour.getHours() + 2); // Correct time returned by API
+
+  for (const hour of priceData.Point) {
+    // Increment time by 1 hour for each datapoint in sequence
+    const timestamp = new Date(
+      firstHour.getTime() + hour.position * 60 * 60 * 1000,
+    );
+
+    hoursArray.push({
+      timestamp: timestamp.toISOString(),
+      price: (hour["price.amount"] / 10) * 1.255, // Convert price to cents/kWh and add VAT
+    });
+  }
+
+  const values = hoursArray.map((hour) => [hour.timestamp, hour.price]);
+
+  try {
+    await sql`
+      INSERT INTO price_data (timestamp, price)
+      VALUES ${sql(values)}
+    `;
+
+    return "Price data inserted into database";
+  } catch (error) {
+    throw error;
+  }
+}
