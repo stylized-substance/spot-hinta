dca534dac488d31969853a79ab85646a8c2dda00
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2025-05-16 13:39:40 +0300

    converted sidebar to drawer

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,37 +21,67 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${quicksand.className} antialiased`}>
-        <div className="grid h-screen grid-cols-[1fr_6fr] xl:grid-cols-[1fr_12fr]">
-          {/* Sidebar*/}
-          <aside className="mt-4 grid grid-rows-[auto_1fr] gap-5 overflow-hidden">
-            <div className="grid justify-items-center">
-              <Image
-                src="/images/lightning-charge-fill-600x600.png"
-                alt="Lightning logo"
-                className="h-16 w-16"
-                height={600}
-                width={600}
-              ></Image>
-              <h1 className="mt-2 text-2xl font-bold">Spot-hinta</h1>
-            </div>
-            <ul className="ms-2 mt-4 text-lg">
-              <li>
-                <Link href="/chart" className="hover:bg-base-300 block p-2">
-                  Chart
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/hourlyprices"
-                  className="hover:bg-base-300 block p-2"
-                >
-                  Hourly prices
-                </Link>
-              </li>
-            </ul>
-          </aside>
-          {/* Main content*/}
-          <main className="overflow-y-auto">{children}</main>
+        <div className="drawer xl:drawer-open">
+          <input id="sidebar" type="checkbox" className="drawer-toggle" />
+          <div className="drawer-content">
+            <label
+              htmlFor="sidebar"
+              className="btn btn-soft btn-square drawer-button xl:hidden"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
+            </label>
+            {/* Main content*/}
+            <main className="overflow-y-auto">{children}</main>
+          </div>
+          <div className="drawer-side">
+            <label
+              htmlFor="sidebar"
+              aria-label="close sidebar"
+              className="drawer-overlay"
+            ></label>
+            {/* Sidebar*/}
+            <aside className="bg-base-100 grid min-h-full grid-rows-[auto_1fr] gap-5 overflow-hidden p-4">
+              <div className="grid justify-items-center">
+                <Image
+                  src="/images/lightning-charge-fill-600x600.png"
+                  alt="Lightning logo"
+                  className="h-16 w-16"
+                  height={600}
+                  width={600}
+                ></Image>
+                <h1 className="mt-2 text-2xl font-bold">Spot-hinta</h1>
+              </div>
+              <ul className="ms-2 mt-4 text-lg">
+                <li>
+                  <Link href="/chart" className="hover:bg-base-300 block p-2">
+                    Chart
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/hourlyprices"
+                    className="hover:bg-base-300 block p-2"
+                  >
+                    Hourly prices
+                  </Link>
+                </li>
+              </ul>
+            </aside>
+          </div>
+          {/* </div> */}
         </div>
       </body>
     </html>
