8bb51dfc391596831a9f14c42d7ff82d4e1aca09
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2025-05-18 17:32:29 +0300

    fetchprices refactoring

--- app/hourlyprices/page.tsx
+++ app/hourlyprices/page.tsx
@@ -2,27 +2,33 @@ import { fetchPrices } from "@/app/lib/fetchPrices";
 import { PriceData } from "@/app/lib/types";
 
 export default async function PriceTable() {
-  const priceData: PriceData = await fetchPrices();
-  
+  // Fetch prices for the last week
+  const priceData: PriceData | [] = await fetchPrices(7);
+  console.log(priceData)
+
   function formatTitle() {
-    const { timestamp }: { timestamp: Date} = priceData[0]
+    const { timestamp }: { timestamp: Date } = priceData[0];
     return timestamp.toLocaleDateString("en-us", {
       year: "numeric",
       month: "long",
-      day: "numeric"
-    })
+      day: "numeric",
+    });
   }
 
   function formatHours(timestamp: Date): string {
-    const startHour = `${timestamp.getUTCHours().toString().padStart(2, "0")}:00`
-    const endHour = `${(timestamp.getUTCHours() + 1).toString().padStart(2, "0")}:00`
-    return (`${startHour} - ${endHour}`)
+    const startHour = `${timestamp.getUTCHours().toString().padStart(2, "0")}:00`;
+    const endHour = `${(timestamp.getUTCHours() + 1).toString().padStart(2, "0")}:00`;
+    return `${startHour} - ${endHour}`;
+  }
+
+  if (priceData.length === 0) {
+    return <div>No price data found</div>;
   }
 
   return (
     <div>
       <h1>{formatTitle()}</h1>
-      <table className="table table-zebra">
+      <table className="table-zebra table">
         <thead>
           <tr>
             <th>Time</th>
--- app/lib/fetchPrices.ts
+++ app/lib/fetchPrices.ts
@@ -1,9 +1,19 @@
 import sql from "@/app/lib/db";
-import { PriceDataSchema } from "@/app/lib/types";
+import { PriceDataSchema, PriceData } from "@/app/lib/types";
 
-export async function fetchPrices() {
+export async function fetchPrices(days = 7): Promise<PriceData | []> {
+  const interval = sql.unsafe(`'${days} days'`);
+  //TODO: fix sql
   try {
-    const priceData = await sql`SELECT id, timestamp, price FROM price_data`;
+    const priceData = await sql`
+    SELECT id, timestamp, price
+    FROM price_data
+    WHERE timestamp BETWEEN
+      (DATE_TRUNC('day', NOW() AT TIME ZONE 'Europe/Helsinki') - INTERVAL ${interval})
+      AND NOW() AT TIME ZONE 'Europe/Helsinki'
+    ORDER BY TIMESTAMP ASC
+    `;
+
     const parsed = PriceDataSchema.parse(priceData);
     return parsed;
   } catch (error) {
