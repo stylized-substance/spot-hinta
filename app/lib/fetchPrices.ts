56d45f979e49502b9ef77d4581e4dab431b023c3
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2025-06-04 13:34:56 +0300

    refactored fetchPrices

--- app/lib/fetchPrices.ts
+++ app/lib/fetchPrices.ts
@@ -1,21 +1,29 @@
 import sql from "@/app/lib/db";
 import { PriceDataArraySchema, PriceDataArray } from "@/app/types/priceData";
+import { DateTime } from "luxon";
 
 // Fetch price data from database
 // Fetching with days = 0 returns prices from beginning of today to end of today
-export async function fetchPrices(days = 0): Promise<PriceDataArray> {
+export async function fetchPrices(days: number): Promise<PriceDataArray> {
   if (days < 0) {
     throw new Error("Interval for price query must be a positive number");
   }
 
-  const interval = sql.unsafe(`'${days} days'`);
-  
+  // Set start time for query to midnight at $days before today and end date to end of today
+  const currentTimeInFinland = DateTime.utc().setZone("Europe/Helsinki");
+  const startTime = currentTimeInFinland
+    .startOf("day")
+    .minus({ days: days })
+    .toUTC()
+    .toISO();
+  const endTime = currentTimeInFinland.endOf("day").toUTC().toISO();
+
   try {
     const priceData = await sql`
     SELECT *
     FROM price_data
-    WHERE timestamp >= DATE_TRUNC('day', NOW() AT TIME ZONE 'Europe/Helsinki') - INTERVAL ${interval}
-    AND timestamp < DATE_TRUNC('day', NOW() AT TIME ZONE 'Europe/Helsinki') + INTERVAL '1 day'
+    WHERE timestamp >= ${startTime}
+    AND timestamp < ${endTime}
     ORDER BY TIMESTAMP ASC
     `;
 
