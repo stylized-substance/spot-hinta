1705b5bb9977804325076d0342d4d71ba210529f
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2025-06-04 12:11:28 +0300

    moved chart theme to separate file

---
+++ app/ui/chart/config.ts
@@ -0,0 +1,111 @@
+// Theme object for LineChart component
+
+export const chartTheme = {
+  text: {
+    fontSize: 12,
+    fill: "#ffffff",
+    outlineWidth: 0,
+    outlineColor: "#000000",
+  },
+  axis: {
+    domain: {
+      line: {
+        stroke: "#777777",
+        strokeWidth: 1,
+      },
+    },
+    legend: {
+      text: {
+        fontSize: 12,
+        fill: "#ffffff",
+        outlineWidth: 0,
+        outlineColor: "#ffffff",
+      },
+    },
+    ticks: {
+      line: {
+        stroke: "#777777",
+        strokeWidth: 1,
+      },
+      text: {
+        fontSize: 12,
+        fill: "#ffffff",
+        outlineWidth: 0,
+        outlineColor: "#ffffff",
+      },
+    },
+  },
+  grid: {
+    line: {
+      stroke: "#3d3846",
+      strokeWidth: 1,
+    },
+  },
+  legends: {
+    title: {
+      text: {
+        fontSize: 12,
+        fill: "#ffffff",
+        outlineWidth: 0,
+        outlineColor: "#ffffff",
+      },
+    },
+    text: {
+      fontSize: 12,
+      fill: "#ffffff",
+      outlineWidth: 0,
+      outlineColor: "#ffffff",
+    },
+    ticks: {
+      line: {},
+      text: {
+        fontSize: 10,
+        fill: "#333333",
+        outlineWidth: 0,
+        outlineColor: "#ffffff",
+      },
+    },
+  },
+  annotations: {
+    text: {
+      fontSize: 13,
+      fill: "#333333",
+      outlineWidth: 2,
+      outlineColor: "#ffffff",
+      outlineOpacity: 1,
+    },
+    link: {
+      stroke: "#000000",
+      strokeWidth: 1,
+      outlineWidth: 2,
+      outlineColor: "#ffffff",
+      outlineOpacity: 1,
+    },
+    outline: {
+      stroke: "#000000",
+      strokeWidth: 2,
+      outlineWidth: 2,
+      outlineColor: "#ffffff",
+      outlineOpacity: 1,
+    },
+    symbol: {
+      fill: "#000000",
+      outlineWidth: 2,
+      outlineColor: "#ffffff",
+      outlineOpacity: 1,
+    },
+  },
+  tooltip: {
+    wrapper: {},
+    container: {
+      background: "#ffffff",
+      color: "#333333",
+      fontSize: 12,
+    },
+    basic: {},
+    chip: {},
+    table: {},
+    tableCell: {},
+    tableCellValue: {},
+  },
+};
