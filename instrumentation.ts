14d0637fcf4ef5885eb466fc82fdcf37cc6a2a74
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2025-06-09 21:26:18 +0300

    added sentry.io logging

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,5 +1,16 @@
+import * as Sentry from "@sentry/nextjs";
 import { runMigrations } from "@/app/lib/db";
 
 export async function register() {
+  if (process.env.NEXT_RUNTIME === "nodejs") {
+    await import("./sentry.server.config");
+  }
+
+  if (process.env.NEXT_RUNTIME === "edge") {
+    await import("./sentry.edge.config");
+  }
+
   await runMigrations();
 }
+
+export const onRequestError = Sentry.captureRequestError;
