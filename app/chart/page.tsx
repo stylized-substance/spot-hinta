0e760ee713e0d06f71edbe65a3c819a432674b6d
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2025-06-23 08:49:10 +0300

    added render time to chart view

--- app/chart/page.tsx
+++ app/chart/page.tsx
@@ -1,31 +1,36 @@
-export const experimental_ppr = true
+export const experimental_ppr = true;
 
 import { Suspense } from "react";
 import PriceStats from "@/app/ui/chart/PriceStats";
 import ChartWrapper from "@/app/ui/chart/ChartWrapper";
+import { DateTime } from "luxon"
 
+const now = DateTime.utc().toISO()
 
 export default async function Page() {
   return (
-    <div className="grid justify-items-center">
-      <Suspense
-        fallback={
-          <div>
-            <span className="loading loading-spinner loading-xl"></span>
-          </div>
-        }
-      >
-        <PriceStats />
-      </Suspense>
-      <Suspense
-        fallback={
-          <div>
-            <span className="loading loading-spinner loading-xl"></span>
-          </div>
-        }
-      >
-        <ChartWrapper />
-      </Suspense>
-    </div>
+    <>
+      <div className="me-4 text-end text-xs md:text-base">Page rendered at {now}</div>
+      <div className="grid justify-items-center">
+        <Suspense
+          fallback={
+            <div>
+              <span className="loading loading-spinner loading-xl"></span>
+            </div>
+          }
+        >
+          <PriceStats />
+        </Suspense>
+        <Suspense
+          fallback={
+            <div>
+              <span className="loading loading-spinner loading-xl"></span>
+            </div>
+          }
+        >
+          <ChartWrapper />
+        </Suspense>
+      </div>
+    </>
   );
 }
