b15a59998bc445eed6b3169f4bbfa6a64406a063
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2025-06-23 09:07:03 +0300

    improved log messages

--- app/lib/fetchWithRetry.ts
+++ app/lib/fetchWithRetry.ts
@@ -9,20 +9,20 @@ export async function fetchWithRetry(
     const response = await fetch(url, options);
 
     if (!response.ok) {
-      throw new Error(`HTTP error: ${response.status}`);
+      throw new Error(`${url}:\n HTTP error: ${response.status}`);
     }
 
     return response;
   } catch (error) {
     if (retries > 0) {
-      console.error("Fetch error:", error);
+      console.error(`${url}:\nFetch error:`, error);
       console.warn(`${url}:\n Fetch failed, retrying`);
       await new Promise((resolve) => setTimeout(resolve, backoff));
 
       return fetchWithRetry(url, retries - 1, options);
     } else {
       console.error(
-        "Fetch failed, maximum retries reached, throwing last error",
+        `${url}:\n: Fetch failed, maximum retries reached`,
       );
 
       throw error;
