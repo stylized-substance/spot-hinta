208d7f6aabce50f3e29593045aa57fd28c6bb029
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2025-06-25 14:22:50 +0300

    daily prices chart working

--- app/types/dailyprices/DailyPrices.ts
+++ app/types/dailyprices/DailyPrices.ts
@@ -1,22 +1,21 @@
 import { PriceDataInFrontend } from "../priceData";
 
 // Price data grouped by year, month and day and average hourly price for each day
-type Day = {
-  day:number, // Day
-  data: {
-    prices: PriceDataInFrontend[];
-    averagePrice: string;
-  }
-}
-
-type Month = {
-  month: number,
-  data: Day[]
-}
-
 type Year = {
   year: number,
   data: Month[]
 }
 
+type Month = {
+  month: string,
+  data: Day[]
+}
+
+type Day = {
+  day: number, // Day
+  data: {
+    prices: PriceDataInFrontend[];
+    averagePrice: string;
+  }
+}
 export type GroupedAndAveraged = Year[]
--- app/ui/dailyprices/DailyPrices.tsx
+++ app/ui/dailyprices/DailyPrices.tsx
@@ -1,11 +1,17 @@
 import { fetchAllPrices } from "@/app/lib/db/fetchPrices";
+import { generatePriceColors } from "@/app/lib/generatePriceColors";
 import { formatPriceData } from "@/app/lib/priceDataProcessor";
 import { findAverageHourPrice } from "@/app/lib/priceDataProcessor";
 import { GroupedAndAveraged } from "@/app/types/dailyprices/DailyPrices";
-
 import { PriceDataArray, PriceDataInFrontend } from "@/app/types/priceData";
+import clsx from "clsx";
+import { DateTime } from "luxon";
 
 export default async function DailyPrices() {
+  // Create DateTime object from current time in Finland. Used for highlighting current day in table
+  const currentTimeInFinland: DateTime =
+    DateTime.now().setZone("Europe/Helsinki");
+
   // Fetch all price data from database
   const priceData: PriceDataArray | [] = await fetchAllPrices();
 
@@ -13,9 +19,10 @@ export default async function DailyPrices() {
   const formattedPriceData: PriceDataInFrontend[] = formatPriceData(priceData);
 
   // Group prices by year, month and day and find average hourly price for each day
-  const grouped = formattedPriceData.reduce(
+  const groupedAndAveraged = formattedPriceData.reduce(
     (acc: GroupedAndAveraged, current: PriceDataInFrontend) => {
-      const { year, month, day } = current.timestamp;
+      const { year, day } = current.timestamp;
+      const month = current.timestamp.monthLong ?? "Unknown";
 
       // Find or create year, month, day
       const yearObject = acc.find((y) => y.year === year);
@@ -58,39 +65,79 @@ export default async function DailyPrices() {
   );
 
   return (
-    <></>
-    // <div className="grid place-items-center">
-    //   <h1 className="mb-8 text-2xl font-bold">Average price per week</h1>
-    //   <div className="w-full max-w-2xl">
-    //     <table className="table">
-    //       <thead>
-    //         <th>Week number</th>
-    //         {allYears.map((year) => (
-    //           <th key={year} className="text-right">
-    //             {year}
-    //           </th>
-    //         ))}
-    //       </thead>
-    //       <tbody>
-    //         {allWeeks.map((weekNumber) => (
-    //           <tr key={weekNumber}>
-    //             <td>{weekNumber}</td>
-    //             {allYears.map((year) => (
-    //               <td
-    //                 key={year}
-    //                 className={
-    //                   generatePriceColors(weekYearPrice[weekNumber][year]) +
-    //                   " text-right"
-    //                 }
-    //               >
-    //                 {weekYearPrice[weekNumber][year]}
-    //               </td>
-    //             ))}
-    //           </tr>
-    //         ))}
-    //       </tbody>
-    //     </table>
-    //   </div>
-    // </div>
+    <div className="grid place-items-center">
+      <h1 className="mb-8 text-2xl font-bold">Average price per day</h1>
+      <div className="w-full max-w-2xl">
+        {groupedAndAveraged.map((year) => {
+          // Get unique months and years for this day
+          const months = year.data.map((m) => m.month);
+          const allDaysSet = new Set<number>();
+          for (const month of year.data) {
+            for (const day of month.data) {
+              allDaysSet.add(day.day);
+            }
+          }
+
+          const allDays = Array.from(allDaysSet).sort((a, b) => a - b);
+
+          return (
+            <div key={year.year}>
+              <h2 className="mb-4 text-center text-2xl font-bold">
+                {year.year}
+              </h2>
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Day</th>
+                    {months.map((month) => (
+                      <th key={month} className="text-right">
+                        {month}
+                      </th>
+                    ))}
+                  </tr>
+                </thead>
+                <tbody>
+                  {allDays.map((dayNumber) => {
+                    const isCurrentDay = dayNumber === currentTimeInFinland.day;
+                    return (
+                      <tr
+                        className={clsx(
+                          isCurrentDay && "border-info border-2 border-dotted",
+                        )}
+                        key={dayNumber}
+                      >
+                        <td>{dayNumber}</td>
+                        {months.map((month) => {
+                          const monthObject = year.data.find(
+                            (m) => m.month === month,
+                          );
+                          const dayObject = monthObject?.data.find(
+                            (d) => d.day === dayNumber,
+                          );
+                          return (
+                            <td
+                              key={month}
+                              className={
+                                dayObject
+                                  ? generatePriceColors(
+                                      dayObject.data.averagePrice,
+                                    ) + " text-right"
+                                  : "text-right"
+                              }
+                            >
+                              {dayObject?.data.averagePrice}
+                            </td>
+                          );
+                        })}
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 }
